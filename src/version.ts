3bfb79d47a0571ebbc5a256c324ba86a9208e155
Liam Monaghan <99554127+lpmsh@users.noreply.github.com>
2026-08-07 12:29:26 -0400

    Update version to 0.18.0

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,5 @@
 // File generated from our OpenAPI spec by Scalar. See README.md for details.
 
-<<<<<<< HEAD
+
 export const VERSION = "0.18.0"; // x-release-please-version
-=======
-export const VERSION = '0.1.0'; // x-release-please-version
->>>>>>> 97b8a7d9ca6f4a45b7a8507a24ca5a511e570608
+
